ec12fa9081231743ca5593e90ff4af879c2be363
naveengitboi <naveenjangiti9@gmail.com>
2023-08-04 19:31:50 +0530

    first init

---
+++ projone/vite-project/src/app.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'preact/hooks'
+import './app.css'
+import * as THREE from 'three'
+export function App() {
+
+  useEffect(() => {
+    const scene = 
+  }, [])
+
+  return (
+    <div>
+      <canvas id='myThreeJsCanvas'></canvas>
+    </div>
+  )
+}
